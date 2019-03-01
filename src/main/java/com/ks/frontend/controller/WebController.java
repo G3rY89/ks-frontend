06f3c9e837d5013ba9ff2468f697144ee4c5de8b
Gergely Kudó <34273369+G3rY89@users.noreply.github.com>
2019-03-01 14:22:13 +0100

    Add files via upload
    ks-frontend for deployment

---
+++ src/main/java/com/ks/frontend/controller/WebController.java
@@ -0,0 +1,43 @@
+package com.ks.frontend.controller;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.core.ParameterizedTypeReference;
+import org.springframework.http.*;
+import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestMethod;
+import org.springframework.web.client.RestTemplate;
+
+import java.util.List;
+
+@Controller
+public class WebController {
+
+    @Autowired
+    private RestTemplate restTemplate;
+
+
+    @RequestMapping(value = "/", method = RequestMethod.GET)
+    public String renderIndex(){
+        return "index";
+    }
+
+    @RequestMapping(value = "/registration", method = RequestMethod.GET)
+    public String registerUser(){
+        return "registration";
+    }
+
+    @RequestMapping(value = "/users", method = RequestMethod.GET)
+    public String showAllUsers(Model model){
+        final String URL = "http://localhost:8762/ks-userservice/users";
+        HttpHeaders headers = new HttpHeaders();
+        headers.set("TOKEN", "QWxhZGRpbjpPcGVuU2VzYW1l");
+        HttpEntity<String> request = new HttpEntity<>(headers);
+        ResponseEntity<List<Object>> responseEntity = restTemplate.exchange(URL, HttpMethod.GET, request, new ParameterizedTypeReference<List<Object>>() {
+        });
+        List<Object> users = responseEntity.getBody();
+        model.addAttribute("users", users);
+        return "users";
+    }
+}
